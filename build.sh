8a05606ec1e7c1352422e43bf6ca9e02da282fb4
Tim Becker <tim@presseverykey.com>
2022-03-09 11:58:19 +0100

    more local

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-mos build --platform esp32 --local --no-libs-update && mos flash
+#mos build --platform esp32 --local --no-libs-update && mos flash
+mos build --local --verbose --deps-dir=../mongoose_quatsch/deps --binary-libs-dir ../mongoose_quatsch/bins --no-libs-update --platform esp32
